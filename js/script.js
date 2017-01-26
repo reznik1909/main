2023d339164919b308bb0942f03656a4ce0b7368
reznik1909 <u.reznik@vabion.ru>
2017-01-26 14:56:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+jQuery(function( $ ){
+	$('#fullpage').fullpage({
+				sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
+				css3: true,
+				navigation: true,
+				navigationPosition: 'right',
+				navigationTooltips: ['First page', 'Second page', 'Third and last page']
+	});
+});
